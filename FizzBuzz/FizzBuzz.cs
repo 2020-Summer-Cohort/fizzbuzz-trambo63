fe62ae83822b2c4b57120fe5ee4f5d8a79aca230
Tom <tomrambo95@yahoo.com>
2020-05-20 14:28:23 -0400

    tests

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Get(int num)
+        {
+            return num.ToString();
+        }
+
+
+    }
+
+
+}
